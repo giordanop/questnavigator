35853fd68a6044c6e1d353cee64d5151146c840b
Giordano Polidoro <g.polidoro@uselab.com>
2024-06-26 18:17:29 +0200

    Custom section and backoffice tour

---
+++ QuestNavigator.Core/BackofficeCustomization/BackofficeTourComposer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.Tour;
+
+namespace QuestNavigator.Core.BackofficeCustomization;
+public class BackofficeTourComposer : IComposer
+{
+	public void Compose(IUmbracoBuilder builder)
+	{
+		// Filter out all the CMS core tours by alias with a Regex that start with the umbIntro alias
+		builder.TourFilters()
+			.AddFilter(new BackOfficeTourFilter(pluginName: null, tourFileName: null, tourAlias: new Regex("^umbIntro", RegexOptions.IgnoreCase)));
+
+		//// Filter any tours in the file that is custom-tours.json
+		//// Found in App_Plugins/MyCustomBackofficeTour/backoffice/tours/
+		//builder.TourFilters()
+		//	.AddFilterByFile("custom-tours"); //Without extension
+
+		// Filter out one or more tour JSON files from a specific plugin/package
+		// Found in App_Plugins/MyCustomBackofficeTour/backoffice/tours/custom-tours.json
+		//builder.TourFilters()
+		//	.AddFilterByPlugin("YourTourPlugin");
+	}
+}
---
+++ QuestNavigator.Core/BackofficeCustomization/MyFavouriteThingsSection.cs
@@ -0,0 +1,21 @@
+﻿using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.Sections;
+
+namespace QuestNavigator.Core.BackofficeCustomization;
+
+public class MyFavouriteThingsSection : ISection
+{
+	public string Alias => "myFavouriteThings";
+
+	public string Name => "My Favourite Things";
+}
+
+public class SectionComposer : IComposer
+{
+	public void Compose(IUmbracoBuilder builder)
+	{
+		builder.Sections().Append<MyFavouriteThingsSection>();
+		//builder.Sections().InsertBefore<SettingsSection, MyFavouriteThingsSection>();
+		//builder.Sections().InsertAfter<SettingsSection, MyFavouriteThingsSection>();
+	}
+}
